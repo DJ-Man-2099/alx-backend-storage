38cd871d72d147f2038e59ea69f245762dd30631
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-13 17:41:56 +0300

    Task 13

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" 13th Task """
+from typing import Dict, List
+from pymongo import MongoClient
+from pymongo.collection import Collection
+
+
+if __name__ == "__main__":
+    """ Main Task """
+    database = "logs"
+    collection = "nginx"
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    methods_mapping = {}
+    logs_collection = client[database][collection]
+    logs = logs_collection.find()
+    logs_count = logs_collection.count_documents({})
+    print(f"{logs_count} logs")
+    print("Methods:")
+    for log in logs:
+        method = log.get("method")
+        if method not in methods_mapping:
+            methods_mapping[method] = 0
+        methods_mapping[method] += 1
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        print(f"\tmethod {method}: {methods_mapping.get(method, 0)}")
+    status_check_count = logs_collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print(f"{status_check_count} status check")
