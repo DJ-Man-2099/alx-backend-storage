fe4306926e2bf2c17035dc82628775530f567f25
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-13 16:49:56 +0300

    Task 10

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 10th Task """
+from typing import Dict, List
+from pymongo.collection import Collection
+from bson import ObjectId
+
+
+def insert_school(mongo_collection: Collection, **kwargs) -> ObjectId:
+    """ List all documents in Python """
+    return mongo_collection.insert_one(kwargs).inserted_id
