da58b9297f992b5a9f00f4dd7942623cbff7f6f1
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-13 17:15:16 +0300

    Task 12

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" 12th Task """
+from typing import Dict, List
+from pymongo.collection import Collection
+
+
+def schools_by_topic(mongo_collection: Collection,
+                     topic: str) -> List[Dict]:
+    """ List all documents in Python """
+    schools = mongo_collection.find({"topics": topic})
+
+    return list(schools)
