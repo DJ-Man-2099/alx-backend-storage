a22049c3b19c6e7e1814579f535c1407f417c2ba
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-13 19:32:48 +0300

    Optional Task 2

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" 2nd Optional Task """
+from typing import Dict, List
+from pymongo.collection import Collection
+
+
+def top_students(mongo_collection: Collection) -> List[Dict]:
+    """ List all documents in Python """
+    return mongo_collection.aggregate([{
+        "$set": {
+            "averageScore": {"$avg": "$topics.score"}
+        },
+    }, {
+        "$sort": {
+            "averageScore": -1
+        }
+    }])
