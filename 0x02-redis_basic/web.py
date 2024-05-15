6fc92a2a403bc7deb0630a451f01e04847f92843
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-15 14:54:54 +0300

    Now???

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,21 @@
 #!/usr/bin/env python3
-""" Optional Task 1 """
-
+"""
+create a web cach
+"""
+import redis
 import requests
-from redis import Redis
-from functools import wraps
-from typing import Any, Callable, Dict, List
+rc = redis.Redis()
+count = 0
 
 
-def track_access(method: Callable) -> Callable:
-    """Tracks Access"""
-    @wraps(method)
-    def wrapper(url: str, *args: List, **kwargs: Dict) -> Any:
-        """Wrapper"""
-        redis_instance = Redis()
-        key = f"count:{url}"
-        cache = f"{url}"
-        redis_instance.incr(key)
-        # count = redis_instance.get(key)
-        response = method(url, *args, **kwargs)
-        redis_instance.setex(cache, 10, response)
-        return response
-    return wrapper
-
-
-@track_access
 def get_page(url: str) -> str:
-    """uses the requests module
-    to obtain the HTML content of a particular URL
-    and returns it"""
-    response = requests.get(url)
-    return response.text
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, resp.text)
+    return resp.text
 
 
 if __name__ == "__main__":
-    """Main function"""
-    get_page("http://slowwly.robertomurray.co.uk")
+    get_page('http://slowwly.robertomurray.co.uk')
