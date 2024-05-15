b6d025cd186e24f85f03d3e1959b0146ca9cbdb0
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-15 14:29:31 +0300

    ????

--- 0x02-redis_basic/optional_main.py
+++ 0x02-redis_basic/optional_main.py
@@ -2,6 +2,7 @@
 """
 Optional Main file
 """
+from time import sleep
 from redis import Redis
 
 get_page = __import__('web').get_page
@@ -12,17 +13,19 @@ redis = Redis()
 url = "http://google.com"
 print(get_page(url))
 print("----------------------")
-url = "http://fb.com"
 
 print(get_page(url))
 print("----------------------")
 print(redis.get(f"count:{url}"))
 
-url = "http://google.com"
 print(get_page(url))
 print("----------------------")
 
 print(get_page(url))
 print("----------------------")
+
+print(redis.get(f"count:{url}"))
+
+sleep(10)
 
 print(redis.get(f"count:{url}"))
