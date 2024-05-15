c7eb45a6478160a35ae01f3844eab1a32b6d5e3c
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-15 15:11:03 +0300

    Correct??

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,39 +1,53 @@
 #!/usr/bin/env python3
-""" Optional Task 1 """
+"""
+get page module
+"""
 
 import requests
 import redis
+from typing import Callable
 from functools import wraps
-from typing import Any, Callable, Dict, List
 
 
-def track_and_cache(method: Callable) -> Callable:
-    """Tracks Access"""
-    @wraps(method)
-    def wrapper(url, *args, **kwargs):
-        """Wrapper"""
-        redis_instance = redis.Redis()
-        key = f"count:{url}"
-        cache = f"{url}"
-        redis_instance.incr(key)
-        cached = redis_instance.get(cache)
-        if cached is not None:
-            return cached.decode('utf-8')
-        response = method(url, *args, **kwargs)
-        redis_instance.setex(cache, 10, response)
-        return response
+def count_access(func: Callable) -> Callable:
+    """
+    Decorator to track URL access
+    Param:
+        call: the call function which is Callable
+
+    Returns:
+        wrapper that tracks the count of access to url
+    """
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        """
+        Retrieves the HTML content of the given URL
+        Params:
+            url: the URL
+        Returns:
+            the HTML retrieved from the URL
+        """
+        redis_client = redis.Redis()
+        redis_client.incr(f"count:{url}")
+
+        cached_content = redis_client.get(url)
+        if cached_content:
+            return cached_content.decode('utf-8')
+        else:
+            response = func(url)
+            redis_client.set(url, 10, response)
+            return response
     return wrapper
 
 
-@track_and_cache
+@count_access
 def get_page(url: str) -> str:
-    """uses the requests module
-    to obtain the HTML content of a particular URL
-    and returns it"""
+    """
+    Fetch the HTML content of a given URL
+    Params:
+        url: the given URL
+    Returns:
+        the HTML retrieved from the URL
+    """
     response = requests.get(url)
     return response.text
-
-
-if __name__ == "__main__":
-    """Main function"""
-    get_page("http://slowwly.robertomurray.co.uk")
